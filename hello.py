633594b7a7517218c3da38980aa70ca0e10c8deb
apollner <46967911+apollner@users.noreply.github.com>
2022-01-05 14:44:20 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,24 @@
 import streamlit as st
+from PIL import Image
+import requests
+
 st.write('# Hello World')
 st.write('## Shalom')
+
+def get_image(url):
+    img = requests.get(url)
+    file = open("sample_image.jpg", "wb")
+    file.write(img.content)
+    file.close()
+    img_file_name = 'sample_image.jpg'
+    return img_file_name
+
+
+# Main driver
+st.title("Image Classification using Vgg16 pretrained Model")
+st.write("Using VGG16 Model to classify the image")
+
+url = st.text_input("Enter Image Url:")
+if url:
+    image = get_image(url)
+    st.image(image)
