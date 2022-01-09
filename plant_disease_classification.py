59d21d03eac2573c493870c8ee3c9851670fd0e8
apollner <46967911+apollner@users.noreply.github.com>
2022-01-09 14:58:50 +0200

    Update plant_disease_classification.py

--- plant_disease_classification.py
+++ plant_disease_classification.py
@@ -49,7 +49,7 @@ class_names=['Apple___Apple_scab',
 
 mobilenet_v3 = tf.keras.models.load_model(('mobilenet_v3_large_100_224.h5'),custom_objects={'KerasLayer':hub.KerasLayer})
 st.write('# Welcome to the PlantVillage leaf disease classifier')
-st.write('###The PlantVillage dataset consist of 38 categories of plants/diseases')
+st.write('### The PlantVillage dataset consist of 38 categories of plants/diseases')
 st.write('#### Paste below the url of a leaf image ')
 
 st.write('It works best with images from this link https://knowyourdata-tfds.withgoogle.com/#tab=STATS&dataset=plant_village')
