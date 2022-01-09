b76f2e6b2277f4293f4644d763e02c0d8b8d5e4f
apollner <46967911+apollner@users.noreply.github.com>
2022-01-09 10:07:11 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,24 +8,68 @@ from keras.applications.vgg16 import preprocess_input
 from keras.applications.vgg16 import decode_predictions
 from keras.applications.vgg16 import VGG16
 from img_classification import classification
+import numpy as np
+
+class_names=['Apple___Apple_scab',
+ 'Apple___Black_rot',
+ 'Apple___Cedar_apple_rust',
+ 'Apple___healthy',
+ 'Blueberry___healthy',
+ 'Cherry_(including_sour)___Powdery_mildew',
+ 'Cherry_(including_sour)___healthy',
+ 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+ 'Corn_(maize)___Common_rust_',
+ 'Corn_(maize)___Northern_Leaf_Blight',
+ 'Corn_(maize)___healthy',
+ 'Grape___Black_rot',
+ 'Grape___Esca_(Black_Measles)',
+ 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+ 'Grape___healthy',
+ 'Orange___Haunglongbing_(Citrus_greening)',
+ 'Peach___Bacterial_spot',
+ 'Peach___healthy',
+ 'Pepper,_bell___Bacterial_spot',
+ 'Pepper,_bell___healthy',
+ 'Potato___Early_blight',
+ 'Potato___Late_blight',
+ 'Potato___healthy',
+ 'Raspberry___healthy',
+ 'Soybean___healthy',
+ 'Squash___Powdery_mildew',
+ 'Strawberry___Leaf_scorch',
+ 'Strawberry___healthy',
+ 'Tomato___Bacterial_spot',
+ 'Tomato___Early_blight',
+ 'Tomato___Late_blight',
+ 'Tomato___Leaf_Mold',
+ 'Tomato___Septoria_leaf_spot',
+ 'Tomato___Spider_mites Two-spotted_spider_mite',
+ 'Tomato___Target_Spot',
+ 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+ 'Tomato___Tomato_mosaic_virus',
+ 'Tomato___healthy']
 
 # To predict the image
 def predict(image1): 
-    model = VGG16()
-    image = load_img(image1, target_size=(224, 224))
-    # convert the image pixels to a numpy array
-    image = img_to_array(image)
-    # reshape data for the model
-    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-    # prepare the image for the VGG model
-    image = preprocess_input(image)
-    # predict the probability across all output classes
-    yhat = model.predict(image)
-    # convert the probabilities to class labels
-    label = decode_predictions(yhat)
-    # retrieve the most likely result, e.g. highest probability
-    label = label[0][0]
-    return label 
+    mobilenet_v3 = tf.keras.models.load_model(('mobilenet_v3_large_100_224.h5'),custom_objects={'KerasLayer':hub.KerasLayer})
+    
+    new_img = tf.keras.preprocessing.image.load_img(image1, target_size=(224, 224))
+    img = tf.keras.preprocessing.image.img_to_array(new_img)
+    img = np.expand_dims(img, axis=0)
+
+
+    #print("Following is our prediction:")
+    prediction = mobilenet_v3.predict(img)
+
+    d = prediction.flatten()
+    j = d.max()
+    for index,item in enumerate(d):
+        if item == j:
+            class_name = class_names[index]
+
+    confidence = round(100 * j, 3)
+    
+    return class_name,confidence  
 
 st.write('# Hello World')
 st.write('## Shalom')
