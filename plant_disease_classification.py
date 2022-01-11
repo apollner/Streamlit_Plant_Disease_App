e570f5b92862b6b785ea964b1f540f6fe5de8cfb
apollner <46967911+apollner@users.noreply.github.com>
2022-01-11 08:59:43 +0200

    Update plant_disease_classification.py

--- plant_disease_classification.py
+++ plant_disease_classification.py
@@ -87,7 +87,7 @@ elif upload:
   if classify:
         st.write("")
         st.write("Classifying...")
-        new_img = tf.keras.preprocessing.image.load_img(bytes_data, target_size=(224, 224))
+        new_img = tf.keras.preprocessing.image.load_img(upload, target_size=(224, 224))
         img = tf.keras.preprocessing.image.img_to_array(new_img)
         img = np.expand_dims(img, axis=0)
         prediction = mobilenet_v3.predict(img)
