00f36c810cfaba55d38ce11e8d8ff08b5d15ff86
apollner <46967911+apollner@users.noreply.github.com>
2022-01-11 10:09:17 +0200

    Update plant_disease_classification.py

--- plant_disease_classification.py
+++ plant_disease_classification.py
@@ -46,9 +46,9 @@ class_names=['Apple___Apple_scab',
  'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
  'Tomato___Tomato_mosaic_virus',
  'Tomato___healthy']
-df = pd.DataFrame(class_names)
-#st.markdown(df)
-df[:]
+df = pd.DataFrame(class_names,index=class_names)
+st.markdown(df.index.tolist())
+#df[:]
 mobilenet_v3 = tf.keras.models.load_model(('mobilenet_v3_large_100_224.h5'),custom_objects={'KerasLayer':hub.KerasLayer})
 st.write('# Welcome to the PlantVillage leaf disease classifier')
 st.write('### The PlantVillage dataset consist of 38 categories of plants/diseases')
