40cc160e5f61c686e66916d5222ee48e37f7c053
apollner <46967911+apollner@users.noreply.github.com>
2022-01-16 22:16:15 +0200

    Update plant_disease_classification.py

--- plant_disease_classification.py
+++ plant_disease_classification.py
@@ -71,7 +71,7 @@ for pic in os.listdir("./leaves_examples/"):
  pic=pic.split(".")[0]
  classes.append(plaintxt(pic))
 
-st.image(pic_list,caption=classes,width=50)
+st.image(pic_list,caption=classes,width=150)
 
 st.write('#### Enter a url or upload an image')
 st.write('For best results use images showing one leaf like the ones here: https://knowyourdata-tfds.withgoogle.com/#tab=STATS&dataset=plant_village')
