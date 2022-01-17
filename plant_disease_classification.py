b75e2f91cca257e6c0701d9defb4d7ecbcac8758
apollner <46967911+apollner@users.noreply.github.com>
2022-01-17 09:27:12 +0200

    Update plant_disease_classification.py

--- plant_disease_classification.py
+++ plant_disease_classification.py
@@ -109,7 +109,7 @@ if url:
  except:
   html_str = f"""<style>p.a {{  font: bold 20px sans-serif;}}</style><p class="a">Error: {sys.exc_info()[0]}%</p>"""
   st.markdown(html_str, unsafe_allow_html=True
-  sys.exit()
+  sys.exit(0)
   #print("Oops!", sys.exc_info()[0], "occurred.")
   #print("Next entry.")
               
